bf53947344ac4c9158dfc937ae9ef2392ec9ecd2
Gian Miguel Del Mundo <gianmiguel98@gmail.com>
2018-12-02 16:47:57 +0800

    Fix indentation

--- User/drivers/tempoencoder.h
+++ User/drivers/tempoencoder.h
@@ -8,9 +8,9 @@
 
 #include <stdint.h>
 
-#define TempoEncoder_Pin1_CLK    RCC_APB2Periph_GPIOC
-#define TempoEncoder_Pin1_PORT   GPIOC
-#define TempoEncoder_Pin1_PIN    GPIO_Pin_6
+#define TempoEncoder_Pin1_CLK     RCC_APB2Periph_GPIOC
+#define TempoEncoder_Pin1_PORT    GPIOC
+#define TempoEncoder_Pin1_PIN     GPIO_Pin_6
 
 #define TempoEncoder_Pin2_CLK     RCC_APB2Periph_GPIOC
 #define TempoEncoder_Pin2_PORT    GPIOC
