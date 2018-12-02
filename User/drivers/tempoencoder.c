2507bacb7ae340535b5bee63703fa4b7a6c1995f
Gian Miguel Del Mundo <gianmiguel98@gmail.com>
2018-12-02 16:08:41 +0800

    Add tempo encoder driver code

--- User/drivers/tempoencoder.c
+++ User/drivers/tempoencoder.c
@@ -1,1 +1,64 @@
 #include "stm32f10x.h"
+
+#include "tempoencoder.h"
+
+static const int8_t EncoderStates[] = { 0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0};
+static void (*EncoderHandler)(int direction) = 0;
+
+static void InitGPIO(void) {
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  RCC_APB2PeriphClockCmd(TempoEncoder_Pin1_CLK, ENABLE);
+  RCC_APB2PeriphClockCmd(TempoEncoder_Pin2_CLK, ENABLE);
+
+  GPIO_InitStruct.GPIO_Pin = TempoEncoder_Pin1_PIN;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IPD;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(TempoEncoder_Pin1_PORT, &GPIO_InitStruct);
+
+  GPIO_InitStruct.GPIO_Pin = TempoEncoder_Pin2_PIN;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IPD;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(TempoEncoder_Pin2_PORT, &GPIO_InitStruct);
+}
+
+static void InitEXTI(void) {
+  EXTI_InitTypeDef EXTI_InitStruct;
+  NVIC_InitTypeDef NVIC_InitStruct_95;
+
+	EXTI_InitStruct.EXTI_Line = EXTI_Line6;
+	EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+	EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+	EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Rising_Falling;
+	EXTI_Init(&EXTI_InitStruct);
+
+  NVIC_InitStruct_95.NVIC_IRQChannel = EXTI9_5_IRQn;
+  NVIC_InitStruct_95.NVIC_IRQChannelPreemptionPriority = 0x00;
+	NVIC_InitStruct_95.NVIC_IRQChannelSubPriority = 0x00;
+	NVIC_InitStruct_95.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStruct_95);
+}
+
+extern void TempoEncoder_Init(void) {
+  InitGPIO();
+  InitEXTI();
+}
+
+extern void TempoEncoder_SetChangeHandler(void handler(int)) {
+  EncoderHandler = handler;
+}
+
+extern void EXTI9_5_IRQHandler(void) {
+  static uint8_t prevState = 0;
+  static uint8_t currState = 0;
+
+  if (EXTI_GetITStatus(EXTI_Line6)) {
+    prevState = prevState << 2;
+    prevState |= (GPIO_ReadInputDataBit(TempoEncoder_Pin2_PORT, TempoEncoder_Pin2_PIN) & 0x03);
+    currState = EncoderStates[prevState & 0x0f];
+
+    if (EncoderHandler) EncoderHandler(currState);
+
+    EXTI_ClearITPendingBit(EXTI_Line6);
+  }
+}
--- User/drivers/tempoencoder.h
+++ User/drivers/tempoencoder.h
@@ -6,6 +6,17 @@
 #ifndef __TEMPOENCODER_H
 #define __TEMPOENCODER_H
 
+#include <stdint.h>
+
+#define TempoEncoder_Pin1_CLK    RCC_APB2Periph_GPIOC
+#define TempoEncoder_Pin1_PORT   GPIOC
+#define TempoEncoder_Pin1_PIN    GPIO_Pin_6
+
+#define TempoEncoder_Pin2_CLK     RCC_APB2Periph_GPIOC
+#define TempoEncoder_Pin2_PORT    GPIOC
+#define TempoEncoder_Pin2_PIN     GPIO_Pin_7
+
 void TempoEncoder_Init(void);
+void TempoEncoder_SetChangeHandler(void handler(int));
 
 #endif
