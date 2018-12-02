36f07bdcbbef58597f46435ae8b1b25d4596b682
Gian Miguel Del Mundo <gianmiguel98@gmail.com>
2018-12-02 16:47:14 +0800

    Add touchscreen pin defines

--- User/drivers/tsc2046.c
+++ User/drivers/tsc2046.c
@@ -1,1 +1,10 @@
 #include "stm32f10x.h"
+
+#include "tsc2046.h"
+
+static void InitGPIO(void) {
+}
+
+extern void TSC2046_Init(void) {
+  InitGPIO();
+}
--- User/drivers/tsc2046.h
+++ User/drivers/tsc2046.h
@@ -6,6 +6,28 @@
 #ifndef __TSC2046_H
 #define __TSC2046_H
 
+#include <stdint.h>
+
+#define TSC2046_DCLK_CLK      RCC_APB2Periph_GPIOE
+#define TSC2046_DCLK_PORT     GPIOE
+#define TSC2046_DCLK_PIN      GPIO_Pin_0
+
+#define TSC2046_CS_CLK        RCC_APB2Periph_GPIOD
+#define TSC2046_CS_PORT       GPIOD
+#define TSC2046_CS_PIN        GPIO_Pin_13
+
+#define TSC2046_DIN_CLK       RCC_APB2Periph_GPIOE
+#define TSC2046_DIN_PORT      GPIOE
+#define TSC2046_DIN_PIN       GPIO_Pin_2
+
+#define TSC2046_DOUT_CLK      RCC_APB2Periph_GPIOE
+#define TSC2046_DOUT_PORT     GPIOE
+#define TSC2046_DOUT_PIN      GPIO_Pin_3
+
+#define TSC2046_PENIRQ_CLK    RCC_APB2Periph_GPIOE
+#define TSC2046_PENIRQ_PORT   GPIOE
+#define TSC2046_PENIRQ_PIN    GPIO_Pin_4
+
 void TSC2046_Init(void);
 
 #endif
