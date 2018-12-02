2b0cc1c7449acc2cfd19b770dad49c1cac8e87e6
Gian Miguel Del Mundo <gianmiguel98@gmail.com>
2018-12-02 17:01:36 +0800

    Add EXTI init for touchscreen

--- User/drivers/tsc2046.c
+++ User/drivers/tsc2046.c
@@ -35,8 +35,34 @@ static void InitGPIO(void) {
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN_FLOATING;
 	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(TSC2046_PENIRQ_PORT, &GPIO_InitStruct);
+
+  GPIO_EXTILineConfig(TSC2046_PENIRQ_PortSource, TSC2046_PENIRQ_PinSource);
+}
+
+static void InitEXTI(void) {
+  EXTI_InitTypeDef EXTI_InitStruct;
+  NVIC_InitTypeDef NVIC_InitStruct;
+
+  EXTI_InitStruct.EXTI_Line = EXTI_Line4;
+  EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+  EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Falling;
+  EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+  EXTI_Init(&EXTI_InitStruct);
+
+  NVIC_InitStruct.NVIC_IRQChannel = EXTI4_IRQn;
+  NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0;
+  NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0;
+  NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStruct);
 }
 
 extern void TSC2046_Init(void) {
   InitGPIO();
+  InitEXTI();
+}
+
+extern void EXTI4_IRQHandler(void) {
+  if (EXTI_GetITStatus(EXTI_Line4)) {
+    EXTI_ClearITPendingBit(EXTI_Line4);
+  }
 }
--- User/drivers/tsc2046.h
+++ User/drivers/tsc2046.h
@@ -8,25 +8,27 @@
 
 #include <stdint.h>
 
-#define TSC2046_DCLK_CLK      RCC_APB2Periph_GPIOE
-#define TSC2046_DCLK_PORT     GPIOE
-#define TSC2046_DCLK_PIN      GPIO_Pin_0
+#define TSC2046_DCLK_CLK            RCC_APB2Periph_GPIOE
+#define TSC2046_DCLK_PORT           GPIOE
+#define TSC2046_DCLK_PIN            GPIO_Pin_0
 
-#define TSC2046_CS_CLK        RCC_APB2Periph_GPIOD
-#define TSC2046_CS_PORT       GPIOD
-#define TSC2046_CS_PIN        GPIO_Pin_13
+#define TSC2046_CS_CLK              RCC_APB2Periph_GPIOD
+#define TSC2046_CS_PORT             GPIOD
+#define TSC2046_CS_PIN              GPIO_Pin_13
 
-#define TSC2046_DIN_CLK       RCC_APB2Periph_GPIOE
-#define TSC2046_DIN_PORT      GPIOE
-#define TSC2046_DIN_PIN       GPIO_Pin_2
+#define TSC2046_DIN_CLK             RCC_APB2Periph_GPIOE
+#define TSC2046_DIN_PORT            GPIOE
+#define TSC2046_DIN_PIN             GPIO_Pin_2
 
-#define TSC2046_DOUT_CLK      RCC_APB2Periph_GPIOE
-#define TSC2046_DOUT_PORT     GPIOE
-#define TSC2046_DOUT_PIN      GPIO_Pin_3
+#define TSC2046_DOUT_CLK            RCC_APB2Periph_GPIOE
+#define TSC2046_DOUT_PORT           GPIOE
+#define TSC2046_DOUT_PIN            GPIO_Pin_3
 
-#define TSC2046_PENIRQ_CLK    RCC_APB2Periph_GPIOE
-#define TSC2046_PENIRQ_PORT   GPIOE
-#define TSC2046_PENIRQ_PIN    GPIO_Pin_4
+#define TSC2046_PENIRQ_CLK          RCC_APB2Periph_GPIOE
+#define TSC2046_PENIRQ_PORT         GPIOE
+#define TSC2046_PENIRQ_PIN          GPIO_Pin_4
+#define TSC2046_PENIRQ_PortSource   GPIO_PortSourceGPIOE
+#define TSC2046_PENIRQ_PinSource    GPIO_PinSource4
 
 void TSC2046_Init(void);
 
