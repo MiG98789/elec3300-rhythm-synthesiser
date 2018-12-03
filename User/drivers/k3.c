ee80a207ca2f503f4f43585e64b2585d2261ef23
Gian Miguel Del Mundo <gianmiguel98@gmail.com>
2018-12-03 22:26:19 +0800

    Add init code for K3

--- User/drivers/k3.c
+++ User/drivers/k3.c
@@ -1,7 +1,42 @@
+#include "stm32f10x.h"
+
 #include "k3.h"
+
+static void InitGPIO(void) {
+  GPIO_InitTypeDef GPIO_InitStruct;
+  
+  RCC_APB2PeriphClockCmd(K3_CLK, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+  
+  GPIO_InitStruct.GPIO_Pin = K3_PIN;
+	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(K3_PORT, &GPIO_InitStruct);
+  GPIO_EXTILineConfig(K3_PortSource, K3_PinSource);
+}
+
+static void InitEXTI(void) {
+  EXTI_InitTypeDef EXTI_InitStruct;
+  NVIC_InitTypeDef NVIC_InitStruct;
+  
+  EXTI_InitStruct.EXTI_Line = EXTI_Line3;
+  EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+  EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Rising;
+	EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+  EXTI_Init(&EXTI_InitStruct);
+  
+  NVIC_InitStruct.NVIC_IRQChannel = EXTI3_IRQn;
+  NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0;
+  NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0;
+  NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStruct);
+}
 
 extern void K3_Init(void) {
   static int init = 0;
   if (init) return;
   else init = 1;
+
+  InitGPIO();
+  InitEXTI();
 }
--- User/drivers/k3.h
+++ User/drivers/k3.h
@@ -6,6 +6,12 @@
 #ifndef __K3_H
 #define __K3_H
 
+#define K3_CLK          RCC_APB2Periph_GPIOA
+#define K3_PORT         GPIOA
+#define K3_PIN          GPIO_Pin_3
+#define K3_PortSource   GPIO_PortSourceGPIOA
+#define K3_PinSource    GPIO_PinSource3
+
 void K3_Init(void);
 
 #endif
